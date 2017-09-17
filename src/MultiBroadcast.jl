751b56e9b8f3aee363770e2cf3dfa9fb531ae4f7
Tamas K. Papp <tkpapp@gmail.com>
2017-09-17 10:58:15 +0200

    Initial commit, some preliminary tests.

--- src/MultiBroadcast.jl
+++ src/MultiBroadcast.jl
@@ -1,5 +1,57 @@
 module MultiBroadcast
 
-# package code goes here
+import Base: size, ndims, eltype, getindex, setindex!, similar
+
+import Base.Broadcast: _broadcast_eltype, broadcast_indices, broadcast!
+
+export multi_broadcast
+
+"""
+    TupleofArrays(arrays...)
+
+Helper structure, maps tuple elements to elements in a tuple of arrays.
+
+The main purpose is to define a `setindex!` method, so that multiple returned
+values from tuples can be placed in the arrays without intermediate allocation.
+
+**This is not part of the interface, just the implementation. Not exported.**
+"""
+struct TupleofArrays{S <: Tuple,T,N} <: AbstractArray{T,N}
+    arrays::S
+    function TupleofArrays(arrays::S) where S
+        @assert !isempty(arrays)
+        siz = size(first(arrays))
+        ixs = indices(first(arrays))
+        for a in Base.tail(arrays)
+            @assert size(a) == siz "Incompatible dimensions"
+            @assert indices(a) == ixs "Incompatible indexing"
+        end
+        T = Tuple{map(eltype, arrays)...}
+        new{S,T,length(siz)}(arrays)
+    end
+end
+
+size(toa::TupleofArrays) = size(toa.arrays[1])
+getindex(toa::TupleofArrays, ixs...) = map(a->a[ixs...], toa.arrays)
+eltype(toa::TupleofArrays{S,T,N}) where {S,T,N} = T
+dims(toa::TupleofArrays{S,T,N}) where {S,T,N} = N
+setindex!(toa::TupleofArrays, value, ixs...) =
+    map((a,v)->setindex!(a, v, ixs...), toa.arrays, value)
+similar(::Type{TupleofArrays}, eltype, shape) =
+    TupleofArrays(map(t->similar(Array{t}, shape), tuple(eltype.parameters...)))
+
+function broadcast!(f, result::NTuple{N,AbstractArray}, args...) where N
+    toa = TupleofArrays(result)
+    broadcast!(f, toa, args...)
+    toa.arrays
+end
+
+function multi_broadcast(f, args...)
+    T = _broadcast_eltype(f, args...)
+    shape = broadcast_indices(args...)
+    toa = similar(TupleofArrays, T, shape)
+    broadcast!(f, toa, args...)
+    toa.arrays
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,11 @@
 using MultiBroadcast
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@testset "multibroadcast vectors" begin
+    x = 1:3
+    y = [5,7,11]
+    f(x, y) = x+y, x*y
+    a, b = multi_broadcast(f, x, y)
+    @test a == x .+ y
+    @test b == x .* y
+end
