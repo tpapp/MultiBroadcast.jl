5223407d5a96aa4ead3d8f3c84d1c38587670d85
Tamas K. Papp <tkpapp@gmail.com>
2017-09-17 11:03:16 +0200

    Added more tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,16 @@
 using MultiBroadcast
 using Base.Test
 
-@testset "multibroadcast vectors" begin
-    x = 1:3
-    y = [5,7,11]
-    f(x, y) = x+y, x*y
+function bivariate_multi_broadcast_test(f, x, y)
     a, b = multi_broadcast(f, x, y)
-    @test a == x .+ y
-    @test b == x .* y
+    @test a == first.(f.(x, y))
+    @test b == last.(f.(x, y))
 end
+
+@testset "multibroadcast arrays" begin
+    f(x, y) = x+y, x*y
+    bivariate_multi_broadcast_test(f, 1:3, [5, 7, 11])
+    bivariate_multi_broadcast_test(f, 1:3, [5, 7, 11]')
+end
+
+
